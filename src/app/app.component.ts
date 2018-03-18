3ca307fec47b6678c7f5e4c5a41cf6e71a069173
Stefan Szabo <stepfan.szabo@gmail.com>
2018-03-18 15:43:33 +0100

    data from code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tiles = [
-    {text: 'Trip 1', location: "Rijeka, Croatia", date: "July 2017"},
-    {text: 'Trip 2', location: "Skopje, Macedonia", date: "August 2017"},
-    {text: 'Trip 3', location: "Sarajevo, Bosnia", date: "August 2017"},
-    {text: 'Trip 4', location: "Bled, Slovenia", date: "September 2017"},
-    {text: 'Trip 5', location: "Rome, Italy", date: "October 2017"}
+  public port_photos = [
+    {text: 'Photo 1', src: "assets/img/photo_1.jpg", origin: "July 2017"},
+    {text: 'Photo 2', src: "assets/img/photo_2.jpg", origin: "July 2017"},
+    {text: 'Photo 3', src: "assets/img/photo_3.jpg", origin: "July 2017"},
+    {text: 'Photo 4', src: "assets/img/photo_4.jpg", origin: "July 2017"},
+    {text: 'Photo 5', src: "assets/img/photo_5.jpg", origin: "July 2017"},
+    {text: 'Photo 6', src: "assets/img/photo_6.jpg", origin: "July 2017"},
+    {text: 'Photo 7', src: "assets/img/photo_7.jpg", origin: "July 2017"},
+    {text: 'Photo 9', src: "assets/img/photo_9.jpg", origin: "July 2017"},
+    {text: 'Photo 10', src: "assets/img/photo_10.jpg", origin: "July 2017"},
   ];
+
+  public personal_data = {
+    name: "Stefan Szabo",
+    email: "stepfan.szabo@gmail.com",
+    address_street: "1000 Street Road",
+    address_city: "Budapest",
+    address_country: "Hungary", 
+    phone: "+36-30-570-9004"
+  };
 }
